e7528be644868a5fc5d4cb6bff641e173b6ef5f3
Emily de França <emydefranca@gmail.com>
2025-08-12 20:01:16 -0300

    atualizações

---
+++ aula12/arrays.js
@@ -0,0 +1,17 @@
+//ALTERAÇÃO DE INDICES
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos.slice(0, -1)); //fatia ou pedaço
+
+/*delete alunos[1];
+console.log(alunos);*/
+
+//const removido = alunos.shift(); //remove do começo
+//const removido = alunos.pop(); //estouro (remove do final)
+//console.log(removido);
+
+//alunos.unshift('Luiza'); -> adiciona no começo
+//alunos.push('Otávio'); -> adicionar no final do array
+
+//console.log(alunos.length);  -> saber o  tamanho do array
+//alunos[alunos.length] = 'Luana' -> adcionar elementos
